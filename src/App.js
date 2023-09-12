b549b4debbe9010734ce3e6604252a3bf8295842
SpiceWeasel66 <nagengasteva@gmail.com>
2023-09-12 09:32:43 +0100

    initial commit
    + added bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      hello{" "}
+      <div className="row">
+        <div className="col-7">hhihihi</div>{" "}
+        <div className="col-5">wdwdqwdqw</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
